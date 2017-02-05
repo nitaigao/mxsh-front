229527577edd4c24d6732ce18823ba7a2ec3a2f9
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-02-05 12:21:23 +0800

    google analytics

--- src/server/server.jsx
+++ src/server/server.jsx
@@ -68,7 +68,15 @@ const template = (preloadedState, html) => {
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>mxsh</title>
         <script src="https://cdn.ravenjs.com/3.10.0/raven.min.js" crossorigin="anonymous"></script>
-        <script>Raven.config('${RAVEN_PUBLIC_DSN}').install();</script>        
+        <script>Raven.config('${RAVEN_PUBLIC_DSN}').install();</script>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-91452305-1', 'auto');
+          ga('send', 'pageview');
+        </script>
       </head>
       <body>
         <div id="react-view">${html}</div>
