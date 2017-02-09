56e4c9db3a9141c5c27cb1222ecaed1121686b09
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-02-09 11:11:54 +0800

    removed ssl check

--- src/server/server.jsx
+++ src/server/server.jsx
@@ -24,12 +24,6 @@ const app                        = express()
 
 if (__PROD__) {
   Raven.config(SENTRY_PRIVATE_DSN).install()
-
-  app.get('*', (req, res) => {
-    if (req.protocol === 'http') {
-      res.redirect(process.env.FRONTEND_HOST)
-    }
-  })
 }
 
 app.use(Raven.requestHandler(SENTRY_PRIVATE_DSN));
