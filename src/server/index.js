a12089907b85428c5c4aebcd70a4a83954074429
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-02-10 08:00:27 +0800

    dont boot ssl in dev

--- src/server/index.js
+++ src/server/index.js
@@ -10,19 +10,23 @@ global.CONFIG = {
 
 const app = require('./server').default
 
-const https = require('https')
 const http  = require('http')
-
-const options = {
-  key: process.env.SSL_KEY.replace(/\\n/g, '\n'),
-  cert: process.env.SSL_CERT.replace(/\\n/g, '\n')
-}
+const https = require('https')
 
 const HTTP_PORT  = process.env.HTTP_PORT || 3000
-const HTTPS_PORT = process.env.HTTPS_PORT || 3443
 
 http.createServer(app).listen(HTTP_PORT, () =>
   console.log('HTTP Listening on port', HTTP_PORT)) // eslint-disable-line no-console
 
-https.createServer(options, app).listen(HTTPS_PORT, () =>
-  console.log('HTTPS Listening on port', HTTPS_PORT)) // eslint-disable-line no-console
+if (process.env.NODE_ENV === 'production') {
+
+  const options = {
+    key: process.env.SSL_KEY.replace(/\\n/g, '\n'),
+    cert: process.env.SSL_CERT.replace(/\\n/g, '\n')
+  }
+
+  const HTTPS_PORT = process.env.HTTPS_PORT || 3443
+
+  https.createServer(options, app).listen(HTTPS_PORT, () =>
+    console.log('HTTPS Listening on port', HTTPS_PORT)) // eslint-disable-line no-console
+}
