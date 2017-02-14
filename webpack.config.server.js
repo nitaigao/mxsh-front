3064ddd8a9ecf016b0c33e40f7170cc390fba89a
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-02-14 14:02:39 +0800

    NODE_ENV again

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -42,20 +42,18 @@ const config = {
 }
 
 if (PRODUCTION) {
-  // config.plugins.push(
-  //   new webpack.DefinePlugin({
-  //     'process.env': {
-  //       NODE_ENV: "'production'"
-  //     }
-  //   })
-  // )
-  // config.plugins.push(
-  //   new webpack.optimize.UglifyJsPlugin({
-  //     compress: {
-  //       warnings: false
-  //     }
-  //   })
-  // )
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': "'production'"
+    })
+  )
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  )
 }
 
 module.exports = config
