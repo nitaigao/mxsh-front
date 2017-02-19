d619663fdc24edbe12ddf5c9b41a61fed6ace779
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-02-19 13:09:17 +0800

    fix the button

--- src/shared/components/Identities.jsx
+++ src/shared/components/Identities.jsx
@@ -52,7 +52,7 @@ class Identities extends Component {
     } else {
       return (
         <div className={styles.newForm}>
-          <h3>New Address</h3>
+          <h2>New Address</h2>
           <button onClick={this.handleNewClick} className='circle proceed'>
             <i className={classNames('icon', 'ion-plus-round', styles.icon)}></i>
           </button>
