fbb577ec26e0f3de304d40c394422a2975423d09
nitaigao <nitaigao@users.noreply.github.com>
2017-04-17 23:52:30 +0800

    Update index.jsx

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,8 +1,8 @@
-import React                              from 'react'
-import { render       }                   from 'react-dom'
+import React            from 'react'
+import { render       } from 'react-dom'
 
-import { AppContainer }                   from 'react-hot-loader'
-import App                                from '../shared/containers/App'
+import { AppContainer } from 'react-hot-loader'
+import App              from '../shared/containers/App'
 
 render(
   <App />,
