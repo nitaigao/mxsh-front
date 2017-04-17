2607f8834af82fc3608306e598477cd8807073ac
nitaigao <nitaigao@users.noreply.github.com>
2017-04-17 23:58:31 +0800

    Update configureStore.js

--- src/shared/configureStore.js
+++ src/shared/configureStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools }          from 'redux-devtools-extension'
-import { routerMiddleware }             from 'react-router-redux'
+import { composeWithDevTools          } from 'redux-devtools-extension'
+import { routerMiddleware             } from 'react-router-redux'
 
 import createSagaMiddleware             from 'redux-saga'
 
