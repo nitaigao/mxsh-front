b216ee9b40f5d1aef92eec7c1b83dd35e6118849
nitaigao <nitaigao@users.noreply.github.com>
2017-04-17 23:57:47 +0800

    Update sagas.js

--- src/shared/modules/sagas.js
+++ src/shared/modules/sagas.js
@@ -1,5 +1,5 @@
-import { saga as authentication }    from './authentication'
-import { saga as identities }        from './identities'
+import { saga as authentication } from './authentication'
+import { saga as identities     } from './identities'
 
 export default function* rootSaga() {
   yield [
