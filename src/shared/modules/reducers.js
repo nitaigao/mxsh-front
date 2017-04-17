95de20af1b4e7199ca765d38689be816e9a790a8
nitaigao <nitaigao@users.noreply.github.com>
2017-04-17 23:57:30 +0800

    Update reducers.js

--- src/shared/modules/reducers.js
+++ src/shared/modules/reducers.js
@@ -1,8 +1,8 @@
-import { combineReducers }              from 'redux'
-import { reducer as formReducer }       from 'redux-form'
+import { combineReducers            } from 'redux'
+import { reducer as formReducer     } from 'redux-form'
 
-import { reducers as authentication }   from './authentication'
-import { reducers as identities }       from './identities'
+import { reducers as authentication } from './authentication'
+import { reducers as identities     } from './identities'
 
 export default combineReducers({
   form: formReducer,
