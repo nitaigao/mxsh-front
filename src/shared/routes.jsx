78f67aa9e25fbbcc8bc2284d9ac0cdd281796b15
nitaigao <nitaigao@users.noreply.github.com>
2017-04-17 23:59:04 +0800

    Update routes.jsx

--- src/shared/routes.jsx
+++ src/shared/routes.jsx
@@ -1,11 +1,12 @@
-import React                 from 'react'
-import { Route, IndexRoute } from 'react-router'
+import React          from 'react'
+import { Route, 
+         IndexRoute } from 'react-router'
 
-import Layout     from './layouts/Layout'
+import Layout         from './layouts/Layout'
 
-import Home       from './components/Home'
-import Authorize  from './components/Authorize'
-import Identities from './components/Identities'
+import Home           from './components/Home'
+import Authorize      from './components/Authorize'
+import Identities     from './components/Identities'
 
 export default (
   <Route path='/' component={Layout}>
